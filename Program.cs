3f3f52ac1f4caa0c012a7b539e3a14dfb51c5317
alexa <alex@trirecords.eu>
2023-07-09 14:46:40 +0200

    init

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿
+
+using System.IO;
+using WinIsTransConsole;
+
+List<string> allWindows = WindowManager.GetAllWindowsAndTheirChildren();
+
+const string fileName = "output.txt";
+
+foreach (string window in allWindows)
+{
+    Console.WriteLine(window);
+    File.AppendAllText(fileName, window + Environment.NewLine);
+}
---
+++ WindowManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Windows.Automation;
+
+namespace WinIsTransConsole;
+
+public static class WindowManager
+{
+    public static List<string> GetWindowsAndChildren(AutomationElement element, int level)
+       {
+           List<string> output = new();
+       
+           // Add the current element's name to the list
+           string indentation = new(' ', level * 4);
+           output.Add($"{indentation}{element.Current.Name}");
+       
+           // Find all children
+           Condition condition = new PropertyCondition(AutomationElement.IsEnabledProperty, true);
+           AutomationElementCollection children = element.FindAll(TreeScope.Children, condition);
+       
+           foreach (AutomationElement child in children)
+           {
+               output.AddRange(GetWindowsAndChildren(child, level + 1));
+           }
+       
+           return output;
+       }
+       
+       public static List<string> GetAllWindowsAndTheirChildren()
+       {
+           List<string> output = new();
+           AutomationElement desktop = AutomationElement.RootElement;
+       
+           if (desktop != null)
+           {
+               Condition condition = new PropertyCondition(AutomationElement.ControlTypeProperty, ControlType.Window);
+               AutomationElementCollection allWindows = desktop.FindAll(TreeScope.Children, condition);
+       
+               foreach (AutomationElement window in allWindows)
+               {
+                   output.AddRange(GetWindowsAndChildren(window, 0));
+               }
+           }
+       
+           return output;
+       }
+}
